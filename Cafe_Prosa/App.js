4381981ae66ebc37cf93c264f096391fb56352bf
Yuri_Puyu <yurikaynandesouzanogueira@gmail.com>
2025-08-13 13:23:33 -0300

    Telas

--- Cafe_Prosa/App.js
+++ Cafe_Prosa/App.js
@@ -1,30 +1,20 @@
-// App.js
-import React from "react";
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-
-// Páginas
-import TelaInicial from "./src/pages/Onboarding";
-import Login from "./src/pages/Login";
-import Cadastro from "./src/pages/Cadastro";
-import EsqueciSenha from "./src/pages/EsqueciSenha";
-import Home from "./src/pages/Home";
-
-const Stack = createNativeStackNavigator();
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator
-        initialRouteName="Onboarding"
-        screenOptions={{ headerShown: false }}
-      >
-        <Stack.Screen name="Onboarding" component={TelaInicial} />
-        <Stack.Screen name="Cadastro" component={Cadastro} />
-        <Stack.Screen name="Login" component={Login} />
-        <Stack.Screen name="EsqueciSenha" component={EsqueciSenha} />
-        <Stack.Screen name="Home" component={Home} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
